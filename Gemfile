9e3063c4aae91a14ff20dfb767e6a29c036802f3
Rémy Hannequin <remy.hannequin@gmail.com>
2017-05-22 15:49:12 +0200

    Add content spec for root endpoint

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ end
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: :mri
+  gem 'json_spec'
   gem 'rspec-rails'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- spec/controllers/root_controller_spec.rb
+++ spec/controllers/root_controller_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe V1::RootController, type: :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it 'contains the greeting message' do
+      get :index
+      json = response.body
+      expect(json).to have_json_path('message')
+      expect(parse_json(json, 'message')).to eq('Hello there!')
+    end
   end
 end
