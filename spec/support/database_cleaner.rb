c9fea120ddf2eea35a28b330e5c46294545e3461
Rémy Hannequin <remy.hannequin@gmail.com>
2017-05-23 19:05:08 +0200

    Change DatabaseCleaner settings

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,14 +1,7 @@
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
+  config.before :suite do
     DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with :truncation
   end
 
   config.before(:each) do
