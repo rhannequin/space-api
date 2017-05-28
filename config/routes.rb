2cab27a5351a244c06e5f0258ce207020ff0b0ce
Rémy Hannequin <remy.hannequin@gmail.com>
2017-05-28 15:23:18 +0200

    Default Stars route to JSON format

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
     root to: 'root#index'
-    resources :stars, only: :index
+    resources :stars, only: :index, defaults: { format: :json }
   end
 end
