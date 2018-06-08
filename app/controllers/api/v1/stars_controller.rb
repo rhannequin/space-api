c9e8fe08e9b1d650aaf18e1830f5663550a833e8
Rémy Hannequin <remy.hannequin@gmail.com>
2018-06-08 13:38:58 +0200

    Use implicit flow with Swagger

--- app/controllers/api/v1/stars_controller.rb
+++ app/controllers/api/v1/stars_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class StarsController < ApiController
+      before_action only: :index do
+        doorkeeper_authorize! :'stars:read'
+      end
+
       def index
         @stars = Star.all
       end
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -54,8 +54,8 @@ Doorkeeper.configure do
   # Define access token scopes for your provider
   # For more information go to
   # https://github.com/doorkeeper-gem/doorkeeper/wiki/Using-Scopes
-  # default_scopes  :public
-  # optional_scopes :write, :update
+  default_scopes  :public
+  optional_scopes :'stars:read'
 
   # Change the way client credentials are retrieved from the request object.
   # By default it retrieves first from the `HTTP_AUTHORIZATION` header, then
@@ -114,7 +114,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.2
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
-  # grant_flows %w[authorization_code client_credentials]
+  grant_flows %w[authorization_code implicit password client_credentials]
 
   # Hook into the strategies' request & response life-cycle in case your
   # application needs advanced customization or logging:
